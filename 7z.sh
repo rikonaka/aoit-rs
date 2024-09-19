9fee14d490cc4816b0abc82d59371bc4aa710db6
RikoNaka <xxy1836@gmail.com>
2024-09-19 17:19:15 +0800

    Add 7z script

---
+++ 7z.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+rm aoit-x86_64-unknown-linux-musl.7z
+rm aoit-aarch64-unknown-linux-musl.7z
+rm aoit-i586-unknown-linux-musl.7z
+
+7z a -mx9 aoit-x86_64-unknown-linux-musl.7z ./target/x86_64-unknown-linux-musl/release/aoit
+7z a -mx9 aoit-aarch64-unknown-linux-musl.7z ./target/aarch64-unknown-linux-musl/release/aoit
+7z a -mx9 aoit-i586-unknown-linux-musl.7z ./target/i586-unknown-linux-musl/release/aoit
